4581f5f031ae825274d3fb2110ca548c3b88a7af
Jeronim Morina <plattenschieber@gmail.com>
2014-12-18 18:34:14 +0100

    Initial Commit

---
+++ MyFutureHome/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyFutureHome
+//
+//  Created by Jeronim Morina on 18.12.14.
+//  Copyright (c) 2014 SI2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
