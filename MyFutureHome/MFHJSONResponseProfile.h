81740e358921101faaa2ec31a82fdc8913ec9c00
Jeronim Morina <plattenschieber@gmail.com>
2015-02-04 10:56:53 +0100

    add template classes for JSON requests

---
+++ MyFutureHome/MFHJSONResponseProfile.h
@@ -0,0 +1,13 @@
+//
+//  MFHJSONResponseProfile.h
+//  MyFutureHome
+//
+//  Created by Jeronim Morina on 04.02.15.
+//  Copyright (c) 2015 SI2. All rights reserved.
+//
+
+#import "MFHJSONResponse.h"
+
+@interface MFHJSONResponseProfile : MFHJSONResponse
+@property (nonatomic,strong) NSString *phoneId;
+@end
---
+++ MyFutureHome/MFHJSONResponseSettings.h
@@ -0,0 +1,14 @@
+//
+//  MFHJSONResponseSettings.h
+//  MyFutureHome
+//
+//  Created by Jeronim Morina on 04.02.15.
+//  Copyright (c) 2015 SI2. All rights reserved.
+//
+
+#import "MFHJSONResponse.h"
+
+@interface MFHJSONResponseSettings : MFHJSONResponse
+@property (nonatomic,strong) NSString *phoneId;
+@property (nonatomic,strong) NSDictionary *userSettings;
+@end
