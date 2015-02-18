089dca9ce1767898657701d42eed2fdf03291d79
Jeronim Morina <plattenschieber@gmail.com>
2015-02-18 10:52:26 +0100

    add complete user class

---
+++ MyFutureHome/MFHUser.h
@@ -0,0 +1,29 @@
+//
+//  MFHUser.h
+//  MyFutureHome
+//
+//  Created by Jeronim Morina on 18.02.15.
+//  Copyright (c) 2015 SI2. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MFHUser : NSObject
+
+@property NSString* phoneId;
+@property NSString* accessToken;
+@property NSString *firstName;
+@property NSString *name;
+@property NSString *sex;
+@property NSString *job;
+@property NSString *birthdate;
+@property NSString *postalCode;
+@property NSString *children;
+@property NSString *city;
+@property NSString *email;
+@property NSString *emailConfirm;
+@property NSString *lat;
+@property NSString *lng;
+@property NSString *created;
+
+@end
---
+++ MyFutureHome/MFHUser.m
@@ -0,0 +1,13 @@
+//
+//  MFHUser.m
+//  MyFutureHome
+//
+//  Created by Jeronim Morina on 18.02.15.
+//  Copyright (c) 2015 SI2. All rights reserved.
+//
+
+#import "MFHUser.h"
+
+@implementation MFHUser
+
+@end
