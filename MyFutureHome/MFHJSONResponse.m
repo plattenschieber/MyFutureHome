9309365fd3ffde8c2849b8010d1ea32066d6c533
Jeronim Morina <plattenschieber@gmail.com>
2015-02-18 20:08:54 +0100

    add implementation to compile correctly

---
+++ MyFutureHome/MFHJSONResponse.m
@@ -0,0 +1,18 @@
+//
+//  MFHJSONResponse.m
+//  
+//
+//  Created by Jeronim Morina on 04.02.15.
+//
+//
+
+#import "MFHJSONResponse.h"
+
+@implementation MFHJSONResponse
+
+- (id) init
+{
+    self = [super init];
+    return self;
+}
+@end
