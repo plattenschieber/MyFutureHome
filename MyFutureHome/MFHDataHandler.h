d4485e93bdcef31649724cd8c5cb5e5a709a149b
Jeronim Morina <plattenschieber@gmail.com>
2015-02-18 10:52:37 +0100

    add data handler class

---
+++ MyFutureHome/MFHDataHandler.h
@@ -0,0 +1,13 @@
+//
+//  MFHDataHandler.h
+//  MyFutureHome
+//
+//  Created by Jeronim Morina on 18.02.15.
+//  Copyright (c) 2015 SI2. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MFHDataHandler : NSObject
+
+@end
---
+++ MyFutureHome/MFHDataHandler.m
@@ -0,0 +1,21 @@
+//
+//  MFHDataHandler.m
+//  MyFutureHome
+//
+//  Created by Jeronim Morina on 18.02.15.
+//  Copyright (c) 2015 SI2. All rights reserved.
+//
+
+#import "MFHDataHandler.h"
+#import <RestKit/RestKit.h>
+
+
+@implementation MFHDataHandler
+
+- (NSString *) mfhInit: (NSString*) phoneId
+{
+    
+    return 
+}
+
+@end
