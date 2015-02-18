431a22824a0f92ef3cb7b8c117b79274eec0aa7c
Jeronim Morina <plattenschieber@gmail.com>
2015-02-18 12:13:26 +0100

    create basic method signatures for RESTKIT

--- MyFutureHome/MFHDataHandler.m
+++ MyFutureHome/MFHDataHandler.m
@@ -24,11 +24,20 @@
     
     return self;
 }
-- (NSString *) mfhInit: (NSString*) phoneId
-{
-    
-    return @"Test";
-}
+
+//! given a unique phoneId, register this phoneId in the database and return a simple
+//! user object with an accessToken
+- (MFHUser *) registerUser: (NSString*) phoneId;
+//! here we update the user object in our database
+- (bool) updateUser: (MFHUser *) user;
+//! here we add/update a search profile of a specific user
+- (bool) updateUserSearchProfileOfUser: (MFHUser *) user withUserSearchProfile: (MFHUserSearchProfile *) searchProfile;
+//! here we add/update the users profile settings
+- (bool) updateUserSettingsOfUser: (MFHUser *) user withUserSettings: (MFHUserSettings *) settings;
+
+//! catch all adverts for given searchProfile
+- (NSMutableArray *) getCatalogueOfUser: (MFHUser *) user withUserProfile: (MFHUserSearchProfile *) searchProfile;
+
 
 - (void)configureRestKit
 {
