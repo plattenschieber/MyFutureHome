f1466657512d39b0c88d18bf4523abfcaea40f2a
Jeronim Morina <plattenschieber@gmail.com>
2015-02-18 22:47:40 +0100

    session class is responsible for user and data handling

---
+++ MyFutureHome/MFHSession.h
@@ -0,0 +1,17 @@
+//
+//  MFHSession.h
+//  MyFutureHome
+//
+//  Created by Jeronim Morina on 18.02.15.
+//  Copyright (c) 2015 SI2. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MFHUser.h"
+
+@interface MFHSession : NSObject
+
++ (void) start;
++ (void) printUser;
+
+@end
---
+++ MyFutureHome/MFHSession.m
@@ -0,0 +1,31 @@
+//
+//  MFHSession.m
+//  MyFutureHome
+//
+//  Created by Jeronim Morina on 18.02.15.
+//  Copyright (c) 2015 SI2. All rights reserved.
+//
+
+#import "MFHSession.h"
+#import "MFHDataHandler.h"
+
+@implementation MFHSession
+
+static MFHUser* user;
+static MFHDataHandler *dataHandler;
+
++ (void) start
+{
+    user = [[MFHUser alloc] init];
+    user.phoneId = [NSString stringWithFormat:@"%@%i", @"SUPERMEGAGEILESTOKEN", arc4random_uniform(74)];
+    MFHDataHandler *dataHandler = [[MFHDataHandler alloc] init];
+    [dataHandler registerUser:user withClass:[MFHUser class]];
+}
+
++ (void) printUser
+{
+    NSLog(@"Und nun sind wir wieder zurück in der Session und geben den AT aus: \n");
+    NSLog(@"%@",user.accessToken);
+}
+
+@end
