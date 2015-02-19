29a93d31d1cb833dd6cc3767f30eaad23059209d
Jeronim Morina <plattenschieber@gmail.com>
2015-02-19 15:58:03 +0100

    setup a standard search profile

--- MyFutureHome/MFHUserSearchProfile.m
+++ MyFutureHome/MFHUserSearchProfile.m
@@ -7,12 +7,25 @@
 //
 
 #import "MFHUserSearchProfile.h"
+#import "MFHSession.h"
 
 @implementation MFHUserSearchProfile
 
 - (id) init
 {
     self = [super init];
+    
+    self.searchProfileId = @"1234";
+    self.favoredStreet = @"Peterstr.";
+    self.favoredArea = @"NRW";
+    self.favoredCity = @"Cologne";
+    self.buy = @"No";
+    self.price = @"750";
+    self.balcony = @"Yes";
+    self.size = @"52";
+    self.rooms = @"3";
+    self.lat = @"30.3";
+    self.lng = @"45.4";
     
     return self;
 }
