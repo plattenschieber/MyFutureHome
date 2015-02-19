734ef76b89f56cee786c2a0d5a8f92b68b11bd24
Jeronim Morina <plattenschieber@gmail.com>
2015-02-19 23:54:06 +0100

    add save method

--- MyFutureHome/MFHSession.h
+++ MyFutureHome/MFHSession.h
@@ -8,11 +8,14 @@
 
 #import <Foundation/Foundation.h>
 #import "MFHUser.h"
+#import "ALSResponse.h"
+#import <RestKit/RestKit.h>
 
 @interface MFHSession : NSObject
 
 + (void) start;
 + (void) printUser;
 + (MFHUser *) getCurrentUser;
++ (void) saveResponse:(RKMappingResult *) result;
 
 @end
