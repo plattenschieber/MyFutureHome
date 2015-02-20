dddd99432d3369f1d62c5364042d3e5421104b2e
Jeronim Morina <plattenschieber@gmail.com>
2015-02-20 08:05:27 +0100

    add cell prototype

---
+++ Controllers/MFHAdvertCell.h
@@ -0,0 +1,15 @@
+//
+//  MFHAdvertCell.h
+//  MyFutureHome
+//
+//  Created by Jeronim Morina on 20.02.15.
+//  Copyright (c) 2015 SI2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MFHAdvertCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIButton *advertButton;
+@property (weak, nonatomic) IBOutlet UILabel *advertLabel;
+
+@end
---
+++ Controllers/MFHAdvertCell.m
@@ -0,0 +1,13 @@
+//
+//  MFHAdvertCell.m
+//  MyFutureHome
+//
+//  Created by Jeronim Morina on 20.02.15.
+//  Copyright (c) 2015 SI2. All rights reserved.
+//
+
+#import "MFHAdvertCell.h"
+
+@implementation MFHAdvertCell
+
+@end
